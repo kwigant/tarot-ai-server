cb757a3af984394084c82616d811befad282bf72
Kirsten Wigant <wigantk@gmail.com>
2025-05-22 13:33:28 -0700

    tweak: remove try catch

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const modeljson = "file://./model/model.json";
 const multer = require("multer");
 const fs = require("fs");
 
-const labels = ["The Fool", "The Magician", "The Chariot", "Temperence", "The Hierophant"];
+const labels = [
+  "The Fool",
+  "The Magician",
+  "The Chariot",
+  "Temperence",
+  "The Hierophant",
+];
 /**
  * Process images and send to the tarot AI model for predictions
  * @param {*} imagePath - path of the image that is being uploaded to the tarot card model
@@ -26,32 +32,15 @@ let model;
 })();
 
 async function classifyImage(imagePath) {
-  // Variables
-  let image = null
-  let buffer = null
-  let result = null
-  // connect model
-
   // read image
-  try {
-    image = await Jimp.read(imagePath);
-  } catch (e) {
-    console.error("Failed to load image:", e);
-    return 'Error: image load failure';
-  }
- 
-  try {
-    buffer = await new Promise((resolve, reject) => {
-        image.getBuffer(Jimp.MIME_JPEG, (err, buf) => {
-          if (err) reject(err);
-          else resolve(buf);
-        });
-      });
-  }
-  catch (e) {
-    console.error("Failed to load buffer:", e);
-    return 'Error: buffer load failure';
-  }
+  const image = await Jimp.read(imagePath);
+
+  const buffer = await new Promise((resolve, reject) => {
+    image.getBuffer(Jimp.MIME_JPEG, (err, buf) => {
+      if (err) reject(err);
+      else resolve(buf);
+    });
+  });
 
   // create image tensor
   const tensor = tf.node
@@ -61,29 +50,22 @@ async function classifyImage(imagePath) {
     .div(tf.scalar(255.0))
     .expandDims(0);
 
-  if (!tensor) return 'Error: unable to create image tensor'
+  if (!tensor) return "Error: unable to create image tensor";
 
   // // Make prediction
   const prediction = model.predict(tensor);
-  
-  try {
-    result = await prediction.data();
-    // Map prediction scores to labels
-    const labeledPredictions = Array.from(result).map((score, idx) => ({
-        label: labels[idx],
-        confidence: score,
-    }));
 
-    // Sort by confidence descending
-    labeledPredictions.sort((a, b) => b.confidence - a.confidence);
+  const result = await prediction.data();
+  // Map prediction scores to labels
+  const labeledPredictions = Array.from(result).map((score, idx) => ({
+    label: labels[idx],
+    confidence: score,
+  }));
 
-    return labeledPredictions;
-  }
-  catch (e) {
-    console.error("Failed to get result:", e);
-    return 'Error: prediction load failure';
-  }
- 
+  // Sort by confidence descending
+  labeledPredictions.sort((a, b) => b.confidence - a.confidence);
+
+  return labeledPredictions;
 }
 
 // Configure multer for image upload
@@ -99,16 +81,16 @@ app.post("/predict", upload.single("image"), async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: "No image uploaded" });
   }
-  console.log('File received:', req.file);
+  console.log("File received:", req.file);
 
   try {
     const result = await classifyImage(req.file.path);
     if (result) {
-        console.log('got result', result)
-        // Clean up uploaded file
-        fs.unlinkSync(req.file.path);
-    
-        res.json({ prediction: result });
+      console.log("got result", result);
+      // Clean up uploaded file
+      fs.unlinkSync(req.file.path);
+
+      res.json({ prediction: result });
     }
   } catch (err) {
     console.error(err);
@@ -117,15 +99,14 @@ app.post("/predict", upload.single("image"), async (req, res) => {
 });
 
 app.post("/upload-test", upload.single("image"), (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
-    console.log("Image upload successful:", req.file);
-    res.status(200).json({ message: "Image received", file: req.file });
-  });
-
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  console.log("Image upload successful:", req.file);
+  res.status(200).json({ message: "Image received", file: req.file });
+});
 
 // Run Server
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Tarot AI Server Running on Port ${PORT}`);
 });
